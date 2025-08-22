2b16cb73b26ae25c460c4aff09dc60471575478b
Lodya_zw74@top-academy.ru <Lodya_zw74@top-academy.ru>
2025-08-23 00:34:26 +0500

    Square DONE

---
+++ TheZoo/main.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std;
+
+class Animal
+{
+	//Астрактный класс
+public:
+	virtual void Sound() = 0;
+};
+class Cat :public Animal
+{
+	//Астрактный класс
+};
+class Lion :public Cat
+{
+	//Конкретный класс
+public:
+	void Sound()override
+	{
+		cout << "Pppppp" << endl;
+	}
+};
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	//Animal animal;		//E0322, C2259
+	//Cat cat;
+	Lion lion;
+}
